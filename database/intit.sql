e5d69667085ef07205186330bda0416831ea3745
kritomas <Unknown>
2024-12-24 16:28:26 +0100

    Added mysql schema.

---
+++ database/intit.sql
@@ -0,0 +1,34 @@
+create table User
+(
+	id varchar(36) primary key default UUID(),
+	username varchar(32) not null unique,
+	password_hash varchar(4096) not null
+);
+
+create table Room
+(
+	id varchar(36) primary key default UUID(),
+	is_public bit not null
+);
+
+create table Message
+(
+	id integer primary key auto_increment,
+	Sender_id varchar(36) not null,
+	Room_id varchar(36) not null,
+	creation_date date not null default now(),
+	message text not null,
+
+	foreign key (Sender_id) references User(id) on delete cascade,
+	foreign key (Room_id) references Room(id) on delete cascade
+);
+
+create table Whitelist
+(
+	id integer primary key auto_increment,
+	User_id varchar(36) not null,
+	Room_id varchar(36) not null,
+
+	foreign key (User_id) references User(id) on delete cascade,
+	foreign key (Room_id) references Room(id) on delete cascade
+);
